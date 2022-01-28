2d931bb382f8ed26306af64cf2bda65a05d03e50
ReddyDamodhar <59497797+ReddyDamodhar@users.noreply.github.com>
2022-01-28 17:14:51 +0530

    Add files via upload
    Final phasse1 assessment projects

---
+++ MethodOverloading/LockedMeMain.java
@@ -0,0 +1,18 @@
+package myproject;
+
+public class LockedMeMain {
+	
+		public static void main(String[] args) {
+
+			
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+
+		MenuOptions.printWelcomeScreen("Locker", "Damodhar");
+
+		HandleOption.handleWelcomeScreenInput();
+		}
+		}
+
+
+
